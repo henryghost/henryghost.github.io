056fb2c57a3d771a7ae29ffc5d96173262c3643f
henryghost <henryghost23@gmail.com>
2019-11-01 10:06:19 -0500

    subiendo archivos

---
+++ js/main.js
@@ -0,0 +1,51 @@
+function ocultar1(){
+	document.getElementById('funciones1').style.display="none";
+}
+
+function mostrar1(){
+	document.getElementById('funciones1').style.display="block";
+
+}
+
+function ocultar2(){
+	document.getElementById('funciones2').style.display="none";
+}
+
+function mostrar2(){
+	document.getElementById('funciones2').style.display="block";
+
+}
+
+function ocultar3(){
+	document.getElementById('funciones3').style.display="none";
+}
+
+function mostrar3(){
+	document.getElementById('funciones3').style.display="block";
+
+}
+
+function ocultar4(){
+	document.getElementById('funciones4').style.display="none";
+}
+
+function mostrar4(){
+	document.getElementById('funciones4').style.display="block";
+
+}
+
+function funcionModal1(){
+	document.getElementById('myModal1').style.display="inline-block";
+}
+
+function funcionModal2(){
+	document.getElementById('myModal2').style.display="inline-block";
+}
+
+function cerrarModal1(){
+	document.getElementById('myModal1').style.display="none";
+}
+
+function cerrarModal2(){
+	document.getElementById('myModal2').style.display="none";
+}
